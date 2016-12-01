91c7aca949c154035a714219c0d7449ddbde37a2
5nn <5nn@users.noreply.github.com>
2016-12-01 10:53:50 +0800

    Add files via upload
    mark 广告view，demo

---
+++ ADView.h
@@ -0,0 +1,22 @@
+
+
+#import <UIKit/UIKit.h>
+
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+#define UserDefaults [NSUserDefaults standardUserDefaults]
+static NSString *const adImageName = @"adImageName";
+static NSString *const adUrl = @"adUrl";
+
+@interface ADView : UIView
+
+/** 倒计时（默认3秒） */
+@property (nonatomic,assign) NSUInteger showTime;
+
+/** 初始化方法*/
+- (instancetype)initWithFrame:(CGRect)frame imgUrl:(NSString *)img adUrl:(NSString *)ad clickImg:(void(^)(NSString *clikImgUrl))block;
+
+/** 显示广告页面方法*/
+- (void)show;
+
+@end
